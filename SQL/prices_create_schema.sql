8fec0247c3791cd077e1f57a5b7148fb12479c4e
zamosi <zamosi@gmail.com>
2024-11-21 18:07:36 +0000

    add file branch_scraper_all_files.py and add create table prices

--- SQL/prices_create_schema.sql
+++ SQL/prices_create_schema.sql
@@ -1,11 +1,40 @@
 CREATE SCHEMA RAW_DATA;
 
 CREATE TABLE RAW_DATA.SNIFIM (
-    STORE_ID INT,
-    BIKORET_NO INT,
-    STORE_TYPE INT,
-    STORE_NAME VARCHAR(100),
+    STOREID INT,
+    BIKORETNO INT,
+    STORETYPE INT,
+    STORENAME VARCHAR(100),
     ADDRESS VARCHAR(255),
     CITY VARCHAR(100),
-    ZIP_CODE VARCHAR(20)
+    ZIPCODE VARCHAR(20),
+	file_name varchar(255) NULL,
+	num_reshet bigint NULL,
+	num_snif int NULL,
+	file_date TIMESTAMP NULL
+);
+
+
+CREATE TABLE RAW_DATA.PRICES(
+	PriceUpdateDate TIMESTAMP NULL,
+	ItemCode bigint NULL,
+	ItemType int NULL,
+	ItemName varchar(255) NULL,
+	ManufacturerName varchar(100) NULL,
+	ManufactureCountry varchar(100) NULL,
+	ManufacturerItemDescription varchar(255) NULL,
+	UnitQty varchar(50) NULL,
+	Quantity float NULL,
+	UnitOfMeasure varchar(50) NULL,
+	bIsWeighted int NULL,
+	QtyInPackage varchar(100) NULL,
+	ItemPrice float NULL,
+	UnitOfMeasurePrice float NULL,
+	AllowDiscount int NULL,
+	ItemStatus int NULL,
+	ItemId int NULL,
+	file_name varchar(255) NULL,
+	num_reshet bigint NULL,
+	num_snif int NULL,
+	file_date TIMESTAMP NULL
 );
