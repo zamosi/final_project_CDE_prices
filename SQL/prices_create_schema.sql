72af64301cc793965d70c9b4cd82201a6d78ef01
Alexey Serdtse <alexeyserdtse@gmail.com>
2024-11-23 20:21:44 +0000

    Add test validation for sql schema

--- SQL/prices_create_schema.sql
+++ SQL/prices_create_schema.sql
@@ -1,50 +1,48 @@
 CREATE SCHEMA RAW_DATA;
 
 CREATE TABLE RAW_DATA.SNIFIM (
-    STOREID INT,
-    BIKORETNO INT,
-    STORETYPE INT,
-    STORENAME VARCHAR(100),
-    ADDRESS VARCHAR(255),
-    CITY VARCHAR(100),
-    ZIPCODE VARCHAR(20),
-	file_name varchar(255) NULL,
-	num_reshet bigint NULL,
-	num_snif int NULL,
-	file_date TIMESTAMP NULL
+    storeid INT,
+    bikoretno INT,
+    storetype INT,
+    storename VARCHAR(100),
+    address VARCHAR(255),
+    city VARCHAR(100),
+    zipcode VARCHAR(20),
+    file_name VARCHAR(255) NULL,
+    num_reshet BIGINT NULL,
+    num_snif INT NULL,
+    file_date TIMESTAMP NULL
 );
 
-
-CREATE TABLE RAW_DATA.PRICES(
-	PriceUpdateDate TIMESTAMP NULL,
-	ItemCode bigint NULL,
-	ItemType int NULL,
-	ItemName varchar(255) NULL,
-	ManufacturerName varchar(100) NULL,
-	ManufactureCountry varchar(100) NULL,
-	ManufacturerItemDescription varchar(255) NULL,
-	UnitQty varchar(50) NULL,
-	Quantity float NULL,
-	UnitOfMeasure varchar(50) NULL,
-	bIsWeighted int NULL,
-	QtyInPackage varchar(100) NULL,
-	ItemPrice float NULL,
-	UnitOfMeasurePrice float NULL,
-	AllowDiscount int NULL,
-	ItemStatus int NULL,
-	ItemId int NULL,
-	file_name varchar(255) NULL,
-	num_reshet bigint NULL,
-	num_snif int NULL,
-	file_date TIMESTAMP NULL
+CREATE TABLE RAW_DATA.PRICES (
+    priceupdatedate TIMESTAMP NULL,
+    itemcode BIGINT NULL,
+    itemtype INT NULL,
+    itemname VARCHAR(255) NULL,
+    manufacturername VARCHAR(100) NULL,
+    manufacturecountry VARCHAR(100) NULL,
+    manufactureritemdescription VARCHAR(255) NULL,
+    unitqty VARCHAR(50) NULL,
+    quantity FLOAT NULL,
+    unitofmeasure VARCHAR(50) NULL,
+    bisweighted INT NULL,
+    qtyinpackage VARCHAR(100) NULL,
+    itemprice FLOAT NULL,
+    unitofmeasureprice FLOAT NULL,
+    allowdiscount INT NULL,
+    itemstatus INT NULL,
+    itemid INT NULL,
+    file_name VARCHAR(255) NULL,
+    num_reshet BIGINT NULL,
+    num_snif INT NULL,
+    file_date TIMESTAMP NULL
 );
-
 
 CREATE TABLE RAW_DATA.RESHATOT (
-    reshet_name varchar(255),
-    reshet_num bigint,
-    vendor_website varchar(100),
-    user_name varchar(100),
-    password varchar(255),
-	file_date TIMESTAMP
+    reshet_name VARCHAR(255),
+    reshet_num BIGINT,
+    vendor_website VARCHAR(100),
+    user_name VARCHAR(100),
+    password VARCHAR(255),
+    file_date TIMESTAMP
 );
